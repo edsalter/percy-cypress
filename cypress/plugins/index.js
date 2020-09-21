980d7ee630b9a62c508278d60227895e378d7356
Wil Wilsman <wil@wilwilsman.com>
2020-09-21 17:19:10 -0500

    ✅ Update test harness

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -10,8 +10,6 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-const percyHealthCheck = require("../../task");
-
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
@@ -24,5 +22,4 @@ module.exports = (on, config) => {
       return null;
     }
   });
-  on("task", percyHealthCheck);
 };
